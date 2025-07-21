4f8afbacb9ce20d2cedcbf02abfcff21b4b1a8d9
狂拽酷炫 <nv@bestbuy.com>
2025-07-21 17:42:40 +0800

    save changes b4 cleaning old branch

--- routers/import_to_db.py
+++ routers/import_to_db.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from typing import Annotated
 
 import pandas as pd
@@ -12,9 +12,10 @@ import models
 from models import Tickets
 from database import SessionLocal
 
-from utils.import_data_to_db import import_to_db
+# from utils.import_data_to_db import import_to_db
 from utils.convert_and_import import combine_process_and_import
 from utils.db_util import db_dependency
+from utils.import_data_to_db import insert_rows_from_df
 
 router = APIRouter(
     prefix="/import_to_db",
@@ -22,12 +23,12 @@ router = APIRouter(
 )
 
 
-@router.post("/import")
-async def import_excel_to_db(db: db_dependency, file_path: str = "data/output.xlsx"):
-    try:
-        return import_to_db(db=db, file_path=file_path)
-    except Exception as e:
-        return {"msg": "导入失败", "error": str(e)}
+# @router.post("/import")
+# async def import_excel_to_db(db: db_dependency, file_path: str = "data/output.xlsx"):
+#     try:
+#         return import_to_db(db=db, file_path=file_path)
+#     except Exception as e:
+#         return {"msg": "导入失败", "error": str(e)}
 
 
 
@@ -38,3 +39,20 @@ async def convert_and_import(db: db_dependency, input_path: str = 'DATA/input.xl
         return combine_process_and_import(input_path=input_path, output_path=output_path, db=db)
     except Exception as e:
         return {"msg": "导入失败", "error": str(e)}
+
+@router.post("/upload")
+async def upload_excel(db: db_dependency, file: UploadFile = File(...)):
+    if not file.filename.endswith((".xls", ".xlsx")):
+        raise HTTPException(status_code=400, detail="Invalid file type")
+
+    contents = await file.read()
+
+    # 用 pandas 读取 Excel 文件内容
+    try:
+        df = pd.read_excel(contents)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Failed to parse Excel")
+
+    insert_rows_from_df(df, db)
+
+    return {"status": "插入成功", "rows": len(df)}
